e48a78794442cb3188ab8fc828b3c8e4824805e3
Ianick Noejovich <iakanoe1056@gmail.com>
2022-01-07 20:10:51 -0300

    base project

---
+++ app/src/main/java/io/github/iakanoe/gallerymm/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.iakanoe.gallerymm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.iakanoe.gallerymm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/github/iakanoe/gallerymm/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.iakanoe.gallerymm.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
